5b5bd01c63626e348acff6ebc2eac9df5b80034b
elianaCvargas <carla.vargas.e@gmail.com>
2018-04-16 00:56:24 -0300

    Add files via upload

---
+++ TP_01_2018_Vargas/Entidades/Calculadora.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        public double Operar( Numero numeroUno, Numero numeroDos, String operador)
+        {
+            double retorno = 0;
+            string retornoOperador = ValidarOperador(operador);
+            switch (retornoOperador)
+            { 
+                case "+":
+                   // retorno = Convert.ToDouble(numeroUno + numeroDos);
+                    break;
+            }
+
+            return retorno;
+                  
+        }
+        private static string ValidarOperador( String operador)
+        {
+            string retorno = "";
+            
+            switch (operador)
+            {
+                case "+":
+                    retorno = "+";
+                    return retorno;
+                    break;
+                case "-":
+                    retorno = "-";
+                    return retorno;
+                    break;
+                case "*":
+                    retorno = "*";
+                    return retorno;
+                    break;
+                case "/":
+                    retorno = "/";
+                    return retorno;
+                    break;
+                default:
+                    retorno = "+";
+                    break;
+            }
+            return retorno;
+        }
+
+
+
+    }
+}
